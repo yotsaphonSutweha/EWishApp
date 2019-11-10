0416024c2b838dd94ba421b3c42f2f8d6c0e0472
yotsaphonSutweha <komsan_58@hotmail.com>
2019-11-10 18:13:14 +0000

    worked on the color scheme, completed design for view item activity and started to design the setting activity

---
+++ app/src/main/java/nci/yo/ewishapp/AvailableItemsActivity.java
@@ -0,0 +1,14 @@
+package nci.yo.ewishapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AvailableItemsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_available_items);
+    }
+}
